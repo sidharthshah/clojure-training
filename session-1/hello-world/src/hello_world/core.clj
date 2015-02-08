434399ed51ff5302ba6cde6284cf05306cb6e348
Sidharth Shah <sidharth@fafadiatech.com>
2015-02-08 12:10:04 +0530

    updated code to call a function

--- session-1/hello-world/src/hello_world/core.clj
+++ session-1/hello-world/src/hello_world/core.clj
@@ -1,7 +1,13 @@
 (ns hello-world.core
 	(:gen-class))
 
+(defn hello 
+	[] 
+	(doseq [i (range 10)] 
+		(println "Dada Bhagwan Na Asim Jai Jaikar Ho!")))
+
+
 (defn -main 
 	"I don't do a whole lot."
 	[]
-	(println "Hello, World!"))
+	(hello))
